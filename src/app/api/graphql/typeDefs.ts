54dc9baa575ca607c33501a1524b76c0675855ab
Eetu Soronen <89025008+soronen@users.noreply.github.com>
2023-09-09 10:03:33 +0300

    succesfully api call

--- src/app/api/graphql/typeDefs.ts
+++ src/app/api/graphql/typeDefs.ts
@@ -3,5 +3,12 @@ import { gql } from 'graphql-tag'
 export const typeDefs = gql`
   type Query {
     hello: String
+    user: [User]
+  }
+  type User {
+    id: ID
+    name: String
+    email: String
+    password: String
   }
 `
