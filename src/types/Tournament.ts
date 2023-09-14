845e8702303012e7dfed22f8a62518144305345b
Otto Oksanen <rocksanen@hotmail.com>
2023-09-14 21:03:40 +0300

    posting user works

--- src/types/Tournament.ts
+++ src/types/Tournament.ts
@@ -3,7 +3,6 @@ import { Types } from "mongoose";
 import { Document } from "mongoose";
 
 interface Tournament extends Document {
-    _id: number;
     name: string;
     rules: number[];
     date: Date;
