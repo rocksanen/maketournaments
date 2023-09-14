14eba70583b2e19581f77b20e539183e2ace5947
Otto Oksanen <rocksanen@hotmail.com>
2023-09-14 19:49:50 +0300

    testing

--- src/app/api/graphql/resolvers.ts
+++ src/app/api/graphql/resolvers.ts
@@ -1,13 +1,25 @@
 import userModel from '@/model/userModel'
+import { Query } from 'mongoose'
 
-const fetchUsers = async () => {
-  const users = await userModel.find({})
+const fetchUser = async () => {
+  const users = await userModel.findOne({ email: "kalle@testmail.com" });
   return users
 }
 
+//post user to database for testing purposes
+const postUser = async () => {
+  const user = new userModel({
+    name: "Kalle Kula",
+    email: "kalle@testmail.com",
+    password: "1234",
+  })
+  const newUser = await user.save()
+  return newUser
+}
+
+
 export const resolvers = {
   Query: {
-    hello: () => 'worlldd',
-    //user: fetchUsers,
-  },
+    user: postUser,
+  }
 }
