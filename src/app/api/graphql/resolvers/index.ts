6eda163ad8fa6bfc747f1d3e4a81518215d1ab7b
emilalg <Emil.Algars@metropolia.fi>
2023-09-19 16:47:18 +0300

    match resolver

--- src/app/api/graphql/resolvers/index.ts
+++ src/app/api/graphql/resolvers/index.ts
@@ -1,7 +1,8 @@
 import userResolvers from './userResolver';
 import rulesResolvers from './rulesResolver';
 import tournamentResolvers from './tournamentResolver';
+import matchResolvers from './matchResolver';
 
-const resolvers = [userResolvers, rulesResolvers, tournamentResolvers];
+const resolvers = [userResolvers, rulesResolvers, tournamentResolvers, matchResolvers];
 
 export default resolvers;
