c9952e724d41e3cdf6f11adf4807cb2e0cac5a86
Otto Oksanen <rocksanen@hotmail.com>
2023-09-29 18:58:29 +0300

    testing

--- src/components/navbar/notifications-dropdown.tsx
+++ src/components/navbar/notifications-dropdown.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import {
   Dropdown,
   DropdownItem,
@@ -7,64 +6,19 @@ import {
   DropdownTrigger,
   NavbarItem,
 } from '@nextui-org/react'
+import React, { useEffect, useState } from 'react'
 import { NotificationIcon } from '../icons/navbar/notificationicon'
-import { gql, useMutation } from '@apollo/client'
-
-const SEND_NOTIFICATION = gql`
-  mutation SendNotification($userId: ID!, $notification: NotificationInput!) {
-    sendNotification(userId: $userId, notification: $notification) {
-      success
-      message
-    }
-  }
-`
 
 interface Notification {
   type: string
   message: string
-  documentKey: {
-    _id: string
-    invitations: string[] // Assuming invitations is an array of strings
+  _id: {
+    _data: string
   }
 }
 
-export const NotificationsDropdown = ({ userId }: { userId: string }) => {
-  const [sendNotificationMutation] = useMutation(SEND_NOTIFICATION)
-  const [userNotifications, setNotifications] = useState<Notification[]>([])
-
-  const sendNotification = async (notification: Notification) => {
-    try {
-      const { type, message, documentKey } = notification
-
-      if (!documentKey.invitations || documentKey.invitations.length === 0) {
-        console.error('No invitations found in notification')
-        return
-      }
-
-      const tournamentId = documentKey.invitations[0]
-
-      const { data } = await sendNotificationMutation({
-        variables: {
-          userId,
-          notification: {
-            type,
-            message,
-            documentKey: {
-              _id: tournamentId,
-            },
-          },
-        },
-      })
-
-      if (data.sendNotification.success) {
-        console.log('Notification sent successfully')
-      } else {
-        console.error('Error sending notification:', data.sendNotification.message)
-      }
-    } catch (error) {
-      console.error('Error sending notification:', error)
-    }
-  }
+export const NotificationsDropdown = () => {
+  const [notifications, setNotifications] = useState<Notification[]>([])
 
   useEffect(() => {
     const eventSource = new EventSource('/api/sse')
@@ -74,9 +28,19 @@ export const NotificationsDropdown = ({ userId }: { userId: string }) => {
     }
 
     eventSource.onmessage = (event) => {
-      const data: Notification = JSON.parse(event.data)
-      setNotifications((prevNotifications) => [...prevNotifications, data])
-      sendNotification(data) // Send notification to the server
+      const data = JSON.parse(event.data)
+      const id = data.updateDescription.updatedFields.invitations[0]
+
+      if (id) {
+        setNotifications((prevNotifications) => [
+          ...prevNotifications,
+          {
+            type: data.type, // Assuming type is available in data
+            message: data.message, // Assuming message is available in data
+            _id: { _data: id },
+          },
+        ])
+      }
     }
 
     eventSource.onerror = (error) => {
@@ -87,7 +51,7 @@ export const NotificationsDropdown = ({ userId }: { userId: string }) => {
     return () => {
       eventSource.close()
     }
-  }, [userId]) // Make sure to include userId as a dependency
+  }, [])
 
   return (
     <Dropdown placement="bottom-end">
@@ -98,15 +62,15 @@ export const NotificationsDropdown = ({ userId }: { userId: string }) => {
       </DropdownTrigger>
       <DropdownMenu className="w-80" aria-label="Avatar Actions">
         <DropdownSection title="Notifications">
-          {userNotifications.map((notification, index) => (
+          {notifications.map((notification, index) => (
             <DropdownItem
               key={index}
               classNames={{
                 base: 'py-2',
                 title: 'text-base font-semibold',
               }}
-              description={'You have been invited to tournament: ' + notification.documentKey._id}
-              textValue={notification.documentKey._id}
+              description={notification._id._data}
+              textValue={notification._id._data}
             >
               {notification.type}
             </DropdownItem>
