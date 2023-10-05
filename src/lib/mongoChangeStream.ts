8840f5ae4d7621e7d97c6a45ebad154ef74cde95
Otto Oksanen <rocksanen@hotmail.com>
2023-10-05 12:18:43 +0300

    test4

--- src/lib/mongoChangeStream.ts
+++ src/lib/mongoChangeStream.ts
@@ -2,26 +2,30 @@ import mongoose from 'mongoose'
 import UserModel from '@/models/userModel'
 
 let userId: string
-let filter: any
-let options: any
+let pipeline: any
 
 function setupChangeStream(id: string) {
   const uri = process.env.MONGO_URI || ''
   userId = id
+  console.log(userId, 'Setting up change stream user id')
   mongoose.connect(uri)
 
   console.log('Setting up change stream')
-  filter = [
+  pipeline = [
     {
       $match: {
         'documentKey._id': new mongoose.Types.ObjectId(userId),
       },
     },
+    {
+      $project: {
+        invitations: 1, // Only include the 'invitations' field in the output
+      },
+    },
   ]
-  options = { fullDocument: 'invitations' }
 }
 
-const changeStream = UserModel.watch(filter, options)
+const changeStream = UserModel.watch(pipeline)
 
 changeStream.on('change', (change) => {
   console.log('Change: ', change)
