93c29a02a23339dfe6a89feee8e341799d44721c
Eetu Soronen <89025008+soronen@users.noreply.github.com>
2023-10-06 14:38:17 +0300

    testdb

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,13 @@
 import { connect, connection } from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI || 'null'
+let MONGO_URI = 'null'
+
+if (process.env.ENV !== 'test') {
+  MONGO_URI = process.env.MONGO_URI || 'null'
+}
+if (process.env.ENV === 'test') {
+  MONGO_URI = process.env.MONGO_URI_TEST || 'null'
+}
 
 const options: any = {
   useUnifiedTopology: true,
@@ -10,6 +17,9 @@ const options: any = {
 
 export const connectToDatabase = async () => {
   if (!connection.readyState) {
+    console.log('process.env.MONGO_URI_TEST', process.env.MONGO_URI_TEST)
+    console.log('process.env.MONGO_URI', process.env.MONGO_URI)
+    console.log('usedmongouri', MONGO_URI)
     console.log('Connecting to ', MONGO_URI)
     connect(MONGO_URI, options)
   } else {
