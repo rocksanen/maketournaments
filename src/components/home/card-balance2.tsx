2edaaa69097776f411a5831da041b01371627033
emilalg <Emil.Algars@metropolia.fi>
2023-10-08 15:11:19 +0300

    card 2

--- src/components/home/card-balance2.tsx
+++ src/components/home/card-balance2.tsx
@@ -1,24 +1,61 @@
 import { Card, CardBody } from '@nextui-org/react'
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { useSession } from 'next-auth/react'
 import { Community } from '../icons/community'
 
+const GET_USER_TOURNAMENTS = gql`
+  query TournamentsByUser($userId: ID!) {
+    tournamentsByUser(userId: $userId) {
+      admin {
+        id
+      }
+      date
+    }
+  }
+`
+
 export const CardBalance2 = () => {
-  return (
-    <Card className="xl:max-w-sm bg-default-50 rounded-xl shadow-md px-3 w-full">
-      <CardBody className="py-5">
+  const { data: session } = useSession()
+  const userId = session?.user?.id ?? null
+
+  const { loading, error, data } = useQuery(GET_USER_TOURNAMENTS, {
+    variables: { userId },
+    skip: !userId,
+  })
+
+  let content
+
+  if (loading || !session) {
+    content = <span className="text-default-900">Loading...</span>
+  } else if (error) {
+    content = <span className="text-default-900">Error: {error.message}</span>
+  } else if (!userId) {
+    content = <span className="text-default-900">Not Logged In</span>
+  } else {
+    const ongoingTournaments = data.tournamentsByUser.filter(
+      (tournament) => tournament.admin.id === userId && new Date(tournament.date) > new Date(),
+    ).length
+
+    content = (
+      <>
         <div className="flex gap-2.5">
           <Community />
           <div className="flex flex-col">
-            <span className="text-default-900">Average Tournament finish</span>
-            <span className="text-default-900 text-xs">36 tournaments</span>
+            <span className="text-default-900">Tournaments Administered</span>
+            <span className="text-default-900 text-xs">{ongoingTournaments} Tournaments</span>
           </div>
         </div>
         <div className="flex gap-2.5 py-2 items-center">
-          <span className="text-default-900 text-xl font-semibold">2.45</span>
-          <span className="text-danger text-xs">- 4.5%</span>
+          <span className="text-default-900 text-xl font-semibold">{ongoingTournaments}</span>
         </div>
-        <div className="flex items-center gap-6"></div>
-      </CardBody>
+      </>
+    )
+  }
+
+  return (
+    <Card className="xl:max-w-sm bg-default-50 rounded-xl shadow-md px-3 w-full">
+      <CardBody className="py-5">{content}</CardBody>
     </Card>
   )
 }
