3ebd61a46efba623d3bc49af5f1983488e8a9a5e
Otto Oksanen <rocksanen@hotmail.com>
2023-10-15 22:41:50 +0300

    workingproductiont

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,22 @@
 import { connect, connection } from 'mongoose'
 
-let MONGO_URI = 'null'
+let MONGO_URI: string
 
 if (process.env.ENV !== 'test') {
-  MONGO_URI = process.env.MONGO_URI || 'null'
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI environment variable is not set!')
+  }
+  MONGO_URI = process.env.MONGO_URI
 }
 if (process.env.ENV === 'test') {
-  MONGO_URI = process.env.MONGO_URI_TEST || 'null'
+  if (!process.env.MONGO_URI_TEST) {
+    throw new Error('MONGO_URI_TEST environment variable is not set for testing environment!')
+  }
+  MONGO_URI = process.env.MONGO_URI_TEST
 }
 
 const options: any = {
   useUnifiedTopology: true,
-
   useNewUrlParser: true,
 }
 
